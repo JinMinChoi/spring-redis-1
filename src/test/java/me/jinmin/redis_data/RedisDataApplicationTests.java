c5518facd5afc0257ec7294801b47dc75845c898
JinMinChoi <jinmin2216@naver.com>
2021-11-29 18:53:25 +0900

    feat: commit first

---
+++ src/test/java/me/jinmin/redis_data/RedisDataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jinmin.redis_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
